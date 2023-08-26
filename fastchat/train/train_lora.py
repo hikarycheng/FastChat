2e141e5f4f9bc344d88c82bbbf450ecccab963cc
Hikaru <hikary.cheng@gmail.com>
2023-08-26 23:19:43 +0800

    comment flash-attn module

--- fastchat/train/train_lora.py
+++ fastchat/train/train_lora.py
@@ -34,9 +34,9 @@ from fastchat.train.train import (
     make_supervised_data_module,
 )
 
-from fastchat.train.llama_flash_attn_monkey_patch import (
-    replace_llama_attn_with_flash_attn,
-)
+# from fastchat.train.llama_flash_attn_monkey_patch import (
+#     replace_llama_attn_with_flash_attn,
+# )
 
 
 @dataclass
@@ -112,8 +112,8 @@ def train():
         lora_args,
     ) = parser.parse_args_into_dataclasses()
 
-    if training_args.flash_attn:
-        replace_llama_attn_with_flash_attn()
+    # if training_args.flash_attn:
+    #     replace_llama_attn_with_flash_attn()
 
     device_map = None
     world_size = int(os.environ.get("WORLD_SIZE", 1))
