d3f9a9a5cef420984a1ddd24ff6b5832bad4c05f
Hikaru <hikary.cheng@gmail.com>
2023-08-27 23:38:19 +0800

    add prompt templates

---
+++ fastchat/prompts/math_template.py
@@ -0,0 +1,40 @@
+
+from dataclasses import dataclass
+
+
+@dataclass
+class MathPromptsTemplate:
+    INTERMEDIATE="""This was your previous work (but I haven\'t seen any of it! I only see what you return as final answer):"""
+
+    PREFIX = """Answer the following questions as best you can. You have access to the following tools:"""
+
+    TOOLS  ="""Calculator: Useful for when you need to answer questions about math."""
+
+    FORMAT_INSTRUCTIONS = """The way you use the tools is by specifying a json blob.
+Specifically, this json should have a `action` key (with the name of the tool to use) and a `action_input` key (with the input to the tool going here).
+
+The only values that should be in the "action" field are: Calculator
+
+The $JSON_BLOB should only contain a SINGLE action, do NOT return a list of multiple actions. Here is an example of a valid $JSON_BLOB:
+
+```
+{{{{
+  "action": $TOOL_NAME,
+  "action_input": $INPUT
+}}}}
+```
+
+ALWAYS use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action:
+```
+$JSON_BLOB
+```
+Observation: the result of the action
+... (this Thought/Action/Observation can repeat N times)
+Thought: I now know the final answer
+    Final Answer: the final answer to the original input question"""
+
+    SUFFIX = """Begin! Reminder to always use the exact characters `Final Answer` when responding."""
---
+++ scripts/ds_train_lora_llama2_70b.sh
@@ -0,0 +1,62 @@
+# available_gpus=0,1,2,3,4,5,6,7
+AVAILABLE_GPUS=0,1,2,3,4,5
+DATASET_TYPE=mix
+MODEL_TYPE=vicuna
+
+OUTPUT_TAG=llama2_70b_lora_${DATASET_TYPE}
+
+n_epochs=1
+DATA_ARGS="\
+    --data_path /mnt/data/zjw/datasets/vicuna-dataset \
+    --eval_data_path /mnt/data/zjw/datasets/formatted-dataset \
+    --lazy_preprocess True \
+    --dataset_random_seed 1311 \
+    --model_type $MODEL_TYPE \
+"
+
+LORA_ARGS="\
+    --lora_r 8 \
+    --lora_alpha 16 \
+    --lora_dropout 0.05 \
+    --q_lora False \
+"
+
+
+TRAIN_ARGS="\
+    --model_name_or_path /mnt/data/lpq/glm_ppo/save/sft/llama2_70b-13k-all11-fusion6_fix2_safe \
+    --tf32 True \
+    --fp16 True \
+    --num_train_epochs $n_epochs \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
+    --gradient_accumulation_steps 2 \
+"
+
+NUM_GPUS=$(($(echo $AVAILABLE_GPUS | grep -o "," | wc -l)+1))
+
+echo "Using ${NUM_GPUS} GPUs, GPU ids: ${AVAILABLE_GPUS}"
+
+TIME_TAG=$( date +"%y%m%d%H%M" )
+SAVE_TAG=${OUTPUT_TAG}_${n_epochs}_epochs_$TIME_TAG
+
+PORT=$((($RANDOM % 1000) + 47000))
+
+deepspeed --include=localhost:$AVAILABLE_GPUS --master_port=$PORT fastchat/train/train_lora.py \
+    --deepspeed playground/deepspeed_config_s3.json \
+    --output_dir checkpoints/$SAVE_TAG \
+    --evaluation_strategy "steps" \
+    --eval_steps 500 \
+    --save_strategy "steps" \
+    --save_steps 500 \
+    --save_total_limit 5 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.04 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --model_max_length 2048 \
+    $DATA_ARGS \
+    $LORA_ARGS \
+    $TRAIN_ARGS \
+    --gradient_checkpointing True \
+    --report_to "wandb" \
